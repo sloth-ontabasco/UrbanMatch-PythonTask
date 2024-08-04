72057f0163343569088a0b6cfde3af0b8c8dfdea
Siddharth N <siddharthn02@gmail.com>
2024-08-05 00:17:02 +0530

    final

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from typing import Any
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.encoders import jsonable_encoder
 from pydantic import ValidationError
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.orm import Session, aliased
 from database import SessionLocal, engine, Base
 import models
 import schemas
@@ -49,7 +50,7 @@ def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
     db.refresh(db_user)
 
     for i in added_interests:
-        i.user_id = db_user.id        
+        i.user_id = db_user.id
     db.commit()
 
     db.refresh(db_user)
@@ -106,7 +107,20 @@ def get_user_matches(user_id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
+    
+    ul1 = aliased(models.user_interests)
+    ul2 = aliased(models.user_interests)
 
-    matches = db.query(models.User).filter(
-        models.User.interests.in_(user.interests)).all()
-    return [m.dict() for m in matches]
+    subq = (select(ul1.interest_id)
+            .where(ul1.user_id == user.id)).subquery()
+
+    q = select(ul2.user_id).join(
+        subq, ul2.interest_id == subq.c.interest_id).where(ul2.user_id != user.id)
+
+    matches = db.execute(q).all()
+    res =[]
+    for m in matches:
+        res.append(db.query(models.User).filter(models.User.id == m[0]).first().__dict__)
+    
+    print(res)
+    return res
